0e5d8f535128873c31fd429565101837bf1e241a
if-else-try <2603130900@qq.com>
2020-11-01 20:39:16 +0800

    week6

---
+++ week6/code/6-1.c
@@ -0,0 +1,8 @@
+#include"ch6.h"
+
+int main()
+{
+	printf("Hello World!");
+	while(1);
+	return 0;
+}
---
+++ week6/code/6-2.c
@@ -0,0 +1,13 @@
+#include"ch6.h"
+
+int main()
+{
+
+	int i;
+	for( i = 0;i < 1012; i++)
+	{
+		printf(" ");
+	}
+	printf("Hello World");
+	return 0;
+}
